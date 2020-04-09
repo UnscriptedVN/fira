0b0e7a231414967e8eac3000870e5b4d5c597c97
Marquis Kurt <software@marquiskurt.net>
2020-04-09 09:18:49 -0400

    [api + docs/api] Deprecate usage of renpy module in info

--- docs/api/info.md
+++ docs/api/info.md
@@ -26,9 +26,20 @@ Create a world and player based on a game level.
 {: .no_toc}
 
 - **level** (int): The level number as indicated by the minigame.
+- **fn_path** (str): The path to where the NadiaVM file will be written to. This excludes
+the file name itself.
+- **\*\*kwargs** (dict): Arbitrary keyword arguments.
+
+#### Kwargs
+{: .no_toc}
+
+Coming soon
+{: .label .label-yellow}
+
+- **config_file** (str): The path to the level configuration file, excluding the file name.
 
 #### Returns
 {: .no_toc}
 
 - **info** (tuple): A tuple containing the [`CSPlayer`](./player.html#csplayer) object and the
-    [`CSWorld`](./world.html#csworld) object
+[`CSWorld`](./world.html#csworld) object
--- uvn_fira/api/info.py
+++ uvn_fira/api/info.py
@@ -12,24 +12,33 @@
 #
 """The info submodule contains the utilities to get the world and player information for a given
     level."""
-import renpy                            #pylint:disable=import-error
+
 from .player import CSPlayer
 from .world import CSWorld
 from ..core import CSWorldConfigReader
 from ..core import CSNadiaVMWriter
 
-def get_level_information(level):
+def get_level_information(level, fn_path="", **kwargs):
     """Create a world and player based on a game level.
 
     Arguments:
         level (int): The level number as indicated by the minigame.
+        fn_path (str): The path to where the NadiaVM file will be written to. This excludes
+            the file name itself.
+        **kwargs (dict): Arbitrary keyword arguments.
+
+    Kwargs:
+        config_file (str): The path to the level configuration file, excluding the file name.
 
     Returns:
         info (tuple): A tuple containing the `minigame.api.player.CSPlayer` object and the
             `minigame.api.world.CSWorld` object
     """
-    w_info = CSWorldConfigReader("core/minigame/levels/level%s.toml" % (level))
-    writer = CSNadiaVMWriter(renpy.config.savedir + "/minigame/compiled/lvl%s.nvm" % (level))
+    conf = kwargs["config_file"] + "/level%s.toml" % (toml) \
+        if "config_file" in kwargs \
+            else "core/minigame/levels/level%s.toml" % (level)
+    w_info = CSWorldConfigReader(conf)
+    writer = CSNadiaVMWriter(fn_path + "/lvl%s.nvm" % (level))
     world = CSWorld(from_data=w_info.data)
     player = CSPlayer(in_world=world, vm=writer)
     return player, world
